32342100d06297f392272b594730194288227241
Reniary <54524466+Reniary@users.noreply.github.com>
2021-03-29 11:45:09 +0400

    change_active

--- js/main.js
+++ js/main.js
@@ -1,12 +1,21 @@
-
 const burger = document.querySelector('.menu__btn');
 const navList = document.querySelector('.nav__list');
+const navLink = document.querySelectorAll('.nav__link');
 
 
 burger.addEventListener('click', e => {
     e.preventDefault();
 
-    navList.classList.toggle('hide');
+    navList.classList.toggle('show');
     
+});
+
+navLink.forEach(item => {
+    item.addEventListener('click', function() {
+        for (let i = 0; i < navLink.length; i++) {
+            navLink[i].classList.remove('active'); 
+          }
+          this.classList.add('active');
+    });
 });
 
